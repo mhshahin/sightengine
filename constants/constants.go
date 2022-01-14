16f569ade79d6da551005e3d3651b6664717033f
Mohammad H. Shahin <mh.shahin@snapp.cab>
2022-01-15 00:34:12 +0330

    Added detection models

---
+++ constants/constants.go
@@ -0,0 +1,20 @@
+package constants
+
+// Endpoint is the base URL of Sightengine
+const Endpoint = "https://api.sightengine.com/1.0"
+
+// Detection Models ...
+const (
+	Nudity            = "nudity"
+	WeaponAlcoholDrug = "wad"
+	Offensive         = "offensive"
+	GraphicGore       = "gore"
+	TextContent       = "text-content"
+	EmbeddedText      = "text"
+	FaceAttributes    = "face-attributes"
+	ImageQuality      = "properties"
+	Color             = "properties"
+	Type              = "type"
+	Scammer           = "scam"
+	Celebrity         = "celebrity"
+)
